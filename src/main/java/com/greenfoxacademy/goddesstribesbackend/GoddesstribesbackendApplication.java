5e7ba9520e1e5e4d2fed548887d26a699882c281
Zétényi Emese <zetenyi.emese@gmail.com>
2019-05-10 22:06:38 +0200

    init project

---
+++ src/main/java/com/greenfoxacademy/goddesstribesbackend/GoddesstribesbackendApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.goddesstribesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoddesstribesbackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoddesstribesbackendApplication.class, args);
+  }
+
+}
