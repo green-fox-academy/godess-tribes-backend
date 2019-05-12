bea8fafeabeebdb910239ffe6afd2a16590b4d71
Zétényi Emese <zetenyi.emese@gmail.com>
2019-05-12 18:36:05 +0200

    create UserController class and autowire userService and kingdomService

--- src/main/java/com/greenfoxacademy/goddesstribesbackend/controllers/UserController.java
+++ src/main/java/com/greenfoxacademy/goddesstribesbackend/controllers/UserController.java
@@ -1,7 +1,20 @@
 package com.greenfoxacademy.goddesstribesbackend.controllers;
 
+import com.greenfoxacademy.goddesstribesbackend.services.KingdomService;
+import com.greenfoxacademy.goddesstribesbackend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+  private UserService userService;
+  private KingdomService kingdomService;
+
+  @Autowired
+  public UserController(UserService userService, KingdomService kingdomService) {
+    this.userService = userService;
+    this.kingdomService = kingdomService;
+  }
+
 }
