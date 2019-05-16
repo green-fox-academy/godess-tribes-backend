2557961a0309f3a09affcafe264f190c698b939e
Agnes <agnes.koperveisz@yahoo.co.uk>
2019-05-16 23:32:11 +0200

    GTB - mockEndpoints/resources added

--- src/main/java/com/greenfoxacademy/goddesstribesbackend/controllers/ResourceController.java
+++ src/main/java/com/greenfoxacademy/goddesstribesbackend/controllers/ResourceController.java
@@ -1,7 +1,22 @@
 package com.greenfoxacademy.goddesstribesbackend.controllers;
 
+import com.greenfoxacademy.goddesstribesbackend.models.dtos.ErrorMessage;
+import com.greenfoxacademy.goddesstribesbackend.models.dtos.ResourceDTO;
+import com.greenfoxacademy.goddesstribesbackend.models.dtos.TokenDTO;
+import com.greenfoxacademy.goddesstribesbackend.security.jwt.JWTUtility;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ResourceController {
+
+  @GetMapping("/kingdom/resources")
+  public ResponseEntity<Object> mockListResources(@RequestBody TokenDTO tokenDTO) {
+    if (JWTUtility.parseToken(tokenDTO.getToken()) == null) {
+      return ResponseEntity.status(400).body(new ErrorMessage("Invalid token"));
+    }
+    return ResponseEntity.status(200).body(new ResourceDTO());
+  }
 }
