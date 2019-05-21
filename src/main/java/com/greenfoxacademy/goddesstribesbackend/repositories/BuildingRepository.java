8047ccd24eea81b447b360730f98aef62a67524c
Zétényi Emese <zetenyi.emese@gmail.com>
2019-05-21 18:40:54 +0200

    add custom queries to BuildingRepository and ResoutceRepository

--- src/main/java/com/greenfoxacademy/goddesstribesbackend/repositories/BuildingRepository.java
+++ src/main/java/com/greenfoxacademy/goddesstribesbackend/repositories/BuildingRepository.java
@@ -3,5 +3,9 @@ package com.greenfoxacademy.goddesstribesbackend.repositories;
 import com.greenfoxacademy.goddesstribesbackend.models.entities.Building;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface BuildingRepository extends CrudRepository<Building, Long> {
+  ArrayList<Building> findBuildingsByKingdom_Id(Long kingdomId);
+  int countBuildingsByKingdom_Id(Long kingdomId);
 }
--- src/main/java/com/greenfoxacademy/goddesstribesbackend/repositories/ResourceRepository.java
+++ src/main/java/com/greenfoxacademy/goddesstribesbackend/repositories/ResourceRepository.java
@@ -3,5 +3,8 @@ package com.greenfoxacademy.goddesstribesbackend.repositories;
 import com.greenfoxacademy.goddesstribesbackend.models.entities.Resource;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ResourceRepository extends CrudRepository<Resource, Long> {
+  ArrayList<Resource> findResourcesByTownhall_Kingdom_Id(Long kingdomId);
 }
