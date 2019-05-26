e077cd73684be6db87d3a48bfbfd4b5d64932b82
Zétényi Emese <zetenyi.emese@gmail.com>
2019-05-26 15:50:39 +0200

    create custom query findBuildingByKingdom_IdAndId in BuildingRepository

--- src/main/java/com/greenfoxacademy/goddesstribesbackend/repositories/BuildingRepository.java
+++ src/main/java/com/greenfoxacademy/goddesstribesbackend/repositories/BuildingRepository.java
@@ -4,9 +4,11 @@ import com.greenfoxacademy.goddesstribesbackend.models.entities.Building;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public interface BuildingRepository extends CrudRepository<Building, Long> {
   ArrayList<Building> findAll();
   ArrayList<Building> findBuildingsByKingdom_Id(Long kingdomId);
+  Optional<Building> findBuildingByKingdom_IdAndId(Long kingdomId, Long buildingId);
   int countBuildingsByKingdom_Id(Long kingdomId);
 }
