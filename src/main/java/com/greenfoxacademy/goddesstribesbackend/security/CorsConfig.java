2ab01c9be1003033fcaaabd86bb3744c8a9f31f5
Szuts Judit <judit.szuts.mob@gmail.com>
2019-05-27 23:26:42 +0200

    allowed origns deleted from corsconfig

--- src/main/java/com/greenfoxacademy/goddesstribesbackend/security/CorsConfig.java
+++ src/main/java/com/greenfoxacademy/goddesstribesbackend/security/CorsConfig.java
@@ -13,7 +13,6 @@ public class CorsConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
 
     registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
   }
 
