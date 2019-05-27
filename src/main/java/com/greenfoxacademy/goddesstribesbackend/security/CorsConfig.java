2852a7038a04461d32e9ad42619eec7c59c6db22
Szuts Judit <judit.szuts.mob@gmail.com>
2019-05-27 23:24:07 +0200

    allowed origns and methods add to corsconfig

--- src/main/java/com/greenfoxacademy/goddesstribesbackend/security/CorsConfig.java
+++ src/main/java/com/greenfoxacademy/goddesstribesbackend/security/CorsConfig.java
@@ -11,7 +11,10 @@ public class CorsConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**");
+
+    registry.addMapping("/**")
+            .allowedOrigins("http://localhost:4200")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
   }
 
 }
