4831640316906cffecedf784bbe9dfd9445a6c4c
Zétényi Emese <zetenyi.emese@gmail.com>
2019-06-04 19:56:11 +0200

    create first test with no token

---
+++ src/test/java/com/greenfoxacademy/goddesstribesbackend/controllers/ResourceControllerTest.java
@@ -0,0 +1,44 @@
+package com.greenfoxacademy.goddesstribesbackend.controllers;
+
+import com.greenfoxacademy.goddesstribesbackend.services.KingdomService;
+import com.greenfoxacademy.goddesstribesbackend.services.ProductionService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ResourceController.class)
+public class ResourceControllerTest {
+
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private KingdomService kingdomService;
+
+  @MockBean
+  private ProductionService productionService;
+
+  @Test
+  public void listResourcesShouldReturnUnauthorized_when_noTokenGiven() throws Exception {
+
+    mockMvc.perform(get("/kingdom/resources"))
+            .andExpect(status().is(401));
+
+  }
+
+}
