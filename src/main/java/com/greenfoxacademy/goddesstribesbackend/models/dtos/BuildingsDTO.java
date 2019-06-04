2421f73675575995d1f3c49f1895d3bd78fdb93b
Zétényi Emese <zetenyi.emese@gmail.com>
2019-06-04 10:58:44 +0200

    change field name buildingDTOs to buildings in dto class BuildingsDTO

--- src/main/java/com/greenfoxacademy/goddesstribesbackend/models/dtos/BuildingsDTO.java
+++ src/main/java/com/greenfoxacademy/goddesstribesbackend/models/dtos/BuildingsDTO.java
@@ -7,25 +7,25 @@ import java.util.List;
 
 @SwaggerDefinition
 public class BuildingsDTO {
-  private List<BuildingDTO> buildingDTOS;
+  private List<BuildingDTO> buildings;
 
   public BuildingsDTO() {
   }
 
-  public BuildingsDTO(List<BuildingDTO> buildingDTOS) {
-    this.buildingDTOS = buildingDTOS;
+  public BuildingsDTO(List<BuildingDTO> buildings) {
+    this.buildings = buildings;
   }
 
   public void add(BuildingDTO buildingDTO){
-    buildingDTOS.add(buildingDTO);
+    buildings.add(buildingDTO);
   }
 
-  public List<BuildingDTO> getBuildingDTOS() {
-    return buildingDTOS;
+  public List<BuildingDTO> getBuildings() {
+    return buildings;
   }
 
-  public void setBuildingDTOS(List<BuildingDTO> buildingDTOS) {
-    this.buildingDTOS = buildingDTOS;
+  public void setBuildings(List<BuildingDTO> buildings) {
+    this.buildings = buildings;
   }
 
 }
