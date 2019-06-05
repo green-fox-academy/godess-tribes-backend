798781774c762b3fbdaf33283267f0d5fb83ccd5
Szuts Judit <judit.szuts.mob@gmail.com>
2019-06-05 13:44:02 +0200

    wrong method removed

--- src/main/java/com/greenfoxacademy/goddesstribesbackend/repositories/ProductionBuildingRepository.java
+++ src/main/java/com/greenfoxacademy/goddesstribesbackend/repositories/ProductionBuildingRepository.java
@@ -10,6 +10,5 @@ import java.util.Optional;
 @Repository
 public interface ProductionBuildingRepository extends CrudRepository<ProductionBuilding, Long> {
   ArrayList<ProductionBuilding> findAll();
-  ArrayList<ProductionBuilding> findTownhallsByKingdom_Id(Long kingdomId);
   Optional<ProductionBuilding> findById(Long productionBuildingId);
 }
